497121574fc0d75fbb087a3845b9ab9fe6f274a2
Pappy-Hub <boyshokan@gmail.com>
2022-05-17 04:09:28 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const http = require("http");
+
+//create a server with the HTTP variable
+const server = http.createServer(function (req, res) {
+  //Headers
+
+  res.writeHead(200, { "content-type": "text/plain" }); //Plain text format
+  res.writeHead(200, { "content-type": "application/json" }); //json format
+  res.writeHead(200, { "content-type": "text/html" }); //html format
+
+  //Send back some information
+  // res.end('Hello, World'); //Plain text format
+
+  //json format format
+  // res.end(`
+  //     {
+  //         "name": "Pappy",
+  //         "Job": "Devops",
+  //         "Quote": "Do It Now"
+  //     }
+  // `);
+
+  //html format
+  res.end(`
+    <html>
+      <body style="background: black; text-align: center; color: cyan">
+        <h1>My First nodejs Server Task </h1>
+        <p>I'm loving it</p> 
+      </body
+    </html>    
+    `);
+});
+
+//Create a port
+server.listen(4000, "127.0.0.1");
+
+console.log("Yes, you have created a server!");
